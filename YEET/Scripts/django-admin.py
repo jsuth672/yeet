a64c71ce4c1467e92300cf6c901abada46dc7d9a
nmans433 <nmans433@students.bju.edu>
2019-10-11 15:47:32 -0400

    new project

---
+++ YEET/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Noah Mansfield\Desktop\School\fall2019\cps420\cps420-project-y-e-e-t\YEET\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
