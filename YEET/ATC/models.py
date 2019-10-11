e70861ef742cdb1a3e3f7326d39fe824af5a0e20
nmans433 <nmans433@students.bju.edu>
2019-10-11 15:57:23 -0400

    add database code and class stubs in model

--- YEET/ATC/models.py
+++ YEET/ATC/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class airplane(models.Model):
+    pass
+class airport(models.Model):
+    pass
+class airline(models.Model):
+    pass
--- YEET/YEET/settings.py
+++ YEET/YEET/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'YEET.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ATC',
+        'HOST': os.environ['DBHOST'],
+        'PORT': 5432,
+        'USER': os.environ['PGUSER'],
+        'PASSWORD': os.environ['PGPASS'],
     }
 }
 
