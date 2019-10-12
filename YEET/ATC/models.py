4da63832513460896173441f1b2a69083eaaae66
nmans433 <nmans433@students.bju.edu>
2019-10-12 17:47:23 -0400

    runs empty tests

--- YEET/ATC/models.py
+++ YEET/ATC/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+
 class Role(models.Model):
     name = models.CharField(max_length=100)
     isATC = models.BooleanField(default=False)
     isGate = models.BooleanField(default=False)
 
+
 class User(models.Model):
     name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)  # TODO: add salt and hash
-    role = models.ForeignKey(Role)
+    role = models.ForeignKey(Role, null=True, on_delete=models.SET_NULL)
 
     def hasATCPerm(self):
         return self.role.isATC
@@ -31,23 +33,25 @@ class Airport(models.Model):
         return Gate.objects.get(airport=self)
 
     def getAirlines(self):
-        return Airline.objects.get(airport=self)
+     return Airline.objects.get(airport=self)
+
 
 class Airline(models.Model):
     name = models.CharField(max_length=100)
-    Airport = models.ManyToManyField("Airport")
+    airport = models.ManyToManyField("Airport")
 
     def getPlanes(self):
         return Plane.objects.get(airline=self.id)
+
 
 class Plane(models.Model):
     identifier = models.CharField(max_length=100)
     size = models.CharField(max_length=10)
     currentPassengerCount = models.IntegerField()
     maxPassengerCount = models.IntegerField()
-    airline = models.ForeignKey("Airline")
-    gate = models.OneToOneField("Gate")
-    runway = models.OneToOneField("Runway")
+    airline = models.ForeignKey("Airline",null=True,on_delete=models.SET_NULL)
+    gate = models.OneToOneField("Gate", null=True,on_delete=models.SET_NULL)
+    runway = models.OneToOneField("Runway", null=True,on_delete=models.SET_NULL)
 
     def getAirline(self):
         return self.airline
@@ -56,10 +60,11 @@ class Plane(models.Model):
     def getRunway(self):
         return self.runway
 
+
 class Runway(models.Model):
     identifier = models.CharField(max_length=100)
     size = models.CharField(max_length=10)
-    airport = models.ForeignKey(Airport)
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
 
     def getPlane(self):
         return self.plane
@@ -71,7 +76,7 @@ class Runway(models.Model):
 class Gate(models.Model):
     identifier = models.CharField(max_length=100)
     size = models.CharField(max_length=10)
-    airport = models.ForeignKey(Airport, on_delete=models.CASCADE())
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
 
     def getPlane(self):
         return self.plane
