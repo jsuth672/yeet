c4a2f2c3d8d30271a75111f43fcbd2e92020a4af
Ryan Longacre <ryanlongacre@outlook.com>
2019-10-12 19:55:30 -0400

    Finished unit tests for airline

--- YEET/ATC/test_airline.py
+++ YEET/ATC/test_airline.py
@@ -1,15 +1,19 @@
+from unittest.mock import patch, MagicMock, Mock
 from django.test import TestCase
-from ATC.models import Airline
+from ATC.models import Airline, Plane
+
 
 class TestAirlineGetPlanes(TestCase):
-    def test_set_up(self):
-        # create test objects here
-        pass
 
     def test_assert(self):
-        # assert statements here
-        pass
+        def filter_mock_1(**kwargs):
+            return [Plane(identifier="plane1")]
 
-    def test_tear_down(self):
-        # delete test objects here
-        pass
+        def filter_mock_2(**kwargs):
+            return [Plane(identifier="plane1"), Plane(identifier="plane2")]
+
+        airline = Airline(name="BJUAir")
+        with patch.object(Plane.objects, 'filter', side_effect=filter_mock_1) as mock_method:
+            self.assertEqual(len(airline.getPlanes()), 1)
+        with patch.object(Plane.objects, 'filter', side_effect=filter_mock_2) as mock_method:
+            self.assertEqual(len(airline.getPlanes()), 2)
