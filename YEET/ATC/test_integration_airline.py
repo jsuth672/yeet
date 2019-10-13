789d17b0afd0c074ae47fbffcd4862bfb2239733
Ryan Longacre <ryanlongacre@outlook.com>
2019-10-12 23:03:56 -0400

    Fixed failed integration tests

--- YEET/ATC/test_integration_airline.py
+++ YEET/ATC/test_integration_airline.py
@@ -9,8 +9,10 @@ class TestAirline(TestCase):
         # create airline
         airline = Airline.objects.create(name="BJUAir")
         # two users, one ATC, one not
-        atc = User.objects.create(name="atc", role=Role(isATC=True))
-        not_atc = User.objects.create(name="not atc", role=Role(isATC=False))
+        atc_role = Role.objects.create(isATC=True)
+        not_atc_role = Role.objects.create(isATC=False)
+        atc = User.objects.create(name="atc", role=atc_role)
+        not_atc = User.objects.create(name="not atc", role=not_atc_role)
         # atc user should be able to access the airline
         response = c.post("ATC/airline/get", {'name': airline.name, 'user': atc.id})
         self.assertEqual(response.status_code, 200)
@@ -18,6 +20,8 @@ class TestAirline(TestCase):
         response = c.post("ATC/airline/get", {'name': airline.name, 'user': not_atc.id})
         self.assertEqual(response.status_code, 403)
         # delete from test database
+        atc_role.delete()
+        not_atc_role.delete()
         airline.delete()
         atc.delete()
         not_atc.delete()
@@ -28,8 +32,10 @@ class TestAirline(TestCase):
         airline1 = Airline.objects.create(name="BJUAir")
         airline2 = Airline.objects.create(name="PCCAir")
         # two users, one ATC, one not
-        atc = User.objects.create(name="atc", role=Role(isATC=True))
-        not_atc = User.objects.create(name="not atc", role=Role(isATC=False))
+        atc_role = Role.objects.create(isATC=True)
+        not_atc_role = Role.objects.create(isATC=False)
+        atc = User.objects.create(name="atc", role=atc_role)
+        not_atc = User.objects.create(name="not atc", role=not_atc_role)
         # make sure the not_atc cannot rename the airline
         response = c.post("ATC/airline/edit", {'name': airline1.name, 'user': not_atc.id, 'new_name': "AirBRUINS"})
         self.assertGreater(response.status_code, 399)
@@ -40,6 +46,8 @@ class TestAirline(TestCase):
         response = c.post("ATC/airline/edit", {'name': "AirBRUINS", 'user': atc.id, 'new_name': airline2.name})
         self.assertGreater(response.status_code, 399)
         # delete from test database
+        atc_role.delete()
+        not_atc_role.delete()
         airline1.delete()
         airline2.delete()
         atc.delete()
