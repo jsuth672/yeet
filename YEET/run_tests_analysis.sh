dc4f43ea2927e479ace7c747e9e6871c9f4b411a
jsuth672 <jsuth672@students.bju.edu>
2019-10-23 15:42:44 -0400

    Create run_tests_analysis.sh

---
+++ YEET/run_tests_analysis.sh
@@ -0,0 +1,38 @@
+python manage.py test
+ 
+sudo apt-get install curl software-properties-common
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+sudo apt install -y nodejs
+export SONAR_SCANNER_VERSION=4.0.0.1744
+export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+rm -rf $SONAR_SCANNER_HOME
+mkdir -p $SONAR_SCANNER_HOME
+curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+rm $HOME/.sonar/sonar-scanner.zip
+export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_OPTS="-server"
+ 
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  sonar-scanner \
+  -Dsonar.projectKey=<your project> \
+  -Dsonar.organization=bjucps \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=<your project> \
+  -Dsonar.python.coverage.reportPaths=nosecover.xml \
+  -Dsonar.python.xunit.reportPaths=nosetests.xml
+else
+  echo "checking pull request..."
+  sonar-scanner \
+  -Dsonar.projectKey=<your project> \
+  -Dsonar.organization=bjucps \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=<your login> \
+  -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST \
+  -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH \
+  -Dsonar.pullrequest.base=master \
+  -Dsonar.python.coverage.reportPaths=nosecover.xml \
+  -Dsonar.python.xunit.reportPaths=nosetests.xml
+fi
